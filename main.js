4e1c89457e6dfca7b5ce7af89082677799235d82
Gonzalo <gonza.martinez13@hotmail.com>
2023-11-21 20:46:27 -0300

    22

--- main.js
+++ main.js
@@ -7,7 +7,7 @@ if (numero % 2 == 0) {
 
 // 2- Crear una función que reciba dos números por parámetro e indique en consola que número es mayor, y si ninguno lo es, indicar por consola que son iguales.//
 
-function dosNum(numA, numB) {
+function dosNum() {
   let numA = 3;
   let numB = 2;
   if (numA > numB) {
@@ -23,3 +23,15 @@ function dosNum(numA, numB) {
     return console.log("Los numeros son iguales.");
   }
 }
+
+// 3- Crear una función que reciba un número por parámetro e indique en consola si ese número es múltiplo de 5.//
+
+// 4- Crear una función que reciba un número por parámetro e imprima por consola todos los números desde el 0 hasta llegar a ese número.//
+
+//5 - Crear una función que reciba una palabra y un número por parámetro e imprima por consola  esa palabra la cantidad correspondiente al número indicado. //
+
+// 6 - Crear una función que reciba un array por parámetro e imprima por consola todos los valores de ese array.//
+
+//7 - Crear una función que reciba un array con 10 números e imprima por consola todos los valores de ese array, menos el que se encuentre en la 5ta posición del mismo. Ayuda: Recuerden que el primer índice de un array es "0". //
+
+//8 - Crea una función que reciba un array de números y un número por parámetro e imprima por consola cada número del array multiplicado por el número pasado por parámetro. //
